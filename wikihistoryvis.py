65db9777175b6c9f420f0e2fbaa49e01b5719549
walkerdb <wboyle13@gmail.com>
2016-02-01 01:29:05 -0500

    working prototype, basic data, with autocomplete

--- wikihistoryvis.py
+++ wikihistoryvis.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from flask import Flask, render_template, request, redirect, url_for
 
@@ -25,6 +27,9 @@ def show_user_summary(username):
 
 @app.route('/article/<article>', methods=["GET", "POST"])
 def show_article_summary(article):
+    auth_cookies = log_in()
+
+
     address = "http://si410wiki.sites.uofmhosting.net/api.php?" \
                "action=query&" \
                "prop=revisions&" \
@@ -32,13 +37,33 @@ def show_article_summary(article):
                "rvprop=ids|flags|timestamp|comment|user|userid|size&" \
                "format=json&" \
                "titles={}".format(article)
-    data = requests.get(address)
-    parser = data_parser.Parser()
-    return article
+
+    data = requests.get(address, cookies=auth_cookies).json()
+    print(data)
+    parser = data_parser.Parser(data)
+
+    return render_template("summary.html", revisions=parser.revisions, title=article)
+
+
+def log_in():
+    with open("wikihistoryvis/login_data.txt", mode="r") as f:
+        login_data = json.load(f)
+
+    password = login_data["pass"]
+    username = login_data["user"]
+
+    url = "http://si410wiki.sites.uofmhosting.net/api.php?action=login&lgname={}&lgpassword={}&format=json".format(username, password)
+    r1 = requests.post(url)
+    token = r1.json()['login']['token']
+    confirmation_url = "http://si410wiki.sites.uofmhosting.net/api.php?action=login&lgname={}&lgpassword={}&format=json&lgtoken={}".format(username, password, token)
+    r2 = requests.post(confirmation_url, cookies=r1.cookies)
+
+    return r2.cookies
 
 
 if __name__ == '__main__':
-    app.run()
+    log_in()
+    app.run(debug=True)
 
 # All recent changes:
 # http://si410wiki.sites.uofmhosting.net/api.php?
